75aec395f6faccd2904743b9df29094ea62cf9cd
PedroAlyson <111298049+PedroAlyson@users.noreply.github.com>
2022-09-26 09:17:18 -0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  public name: any[] = [];
+  public nada: any;
+  public outro: any;
+
+  constructor() {
+    for (let i = 0; 6 > i; ++i) {
+      this.name.push({ id: i, name: 'nome ' + i });
+    }
+    console.log(this.name);
+    this.nada = 0;
+    this.outro = 0;
+
+  }
 }
